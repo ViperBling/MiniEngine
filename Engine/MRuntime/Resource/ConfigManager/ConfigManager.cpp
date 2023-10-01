6f5008dfb52697b87cfd5ab71a2bb8377de4fac8
ArtoriasLeo <qiusongtao2010@hotmail.com>
2023-10-01 16:09:35 +0800

    [M] 修改文件结构

---
+++ Engine/MRuntime/Resource/ConfigManager/ConfigManager.cpp
@@ -0,0 +1,45 @@
+#include "Resource/ConfigManager/ConfigManager.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "ConfigManager.hpp"
+
+namespace MiniEngine
+{
+    void MiniEngine::ConfigManager::Initialize(const std::filesystem::path &configFilePath)
+    {
+        std::ifstream configFile(configFilePath);
+
+        std::string configLine;
+        while (std::getline(configFile, configLine))
+        {
+            size_t seperatePos = configLine.find_first_of("=");
+            if (seperatePos > 0 && seperatePos < (configLine.length() - 1))
+            {
+                std::string name = configLine.substr(0, seperatePos);
+                std::string value = configLine.substr(seperatePos + 1, configLine.length() - seperatePos - 1);
+
+                if (name == "BinaryRootFolder")
+                    mRootFolder = configFilePath.parent_path() / value;
+                else if (name == "AssetFolder")
+                    mAssetFolder = mRootFolder / value;
+                else if (name == "SchemaFolder")
+                    mSchemaFolder = mRootFolder / value;
+                else if (name == "DefaultWorld")
+                    mDefaultWorldURL = value;
+                else if (name == "BigIconFile")
+                    mEditorBigIconPath = mRootFolder / value;
+                else if (name == "SmallIconFile")
+                    mEditorSmallIconPath = mRootFolder / value;
+                else if (name == "FontFile")
+                    mEditorFontPath = mRootFolder / value;
+                else if (name == "GlobalRenderingRes")
+                    mGlobalRenderingResURL = value;
+                else if (name == "GlobalParticleRes")
+                    mGlobalParticalResURL = value;
+            }
+        }
+    }
+} // namespace MiniEngine
+
